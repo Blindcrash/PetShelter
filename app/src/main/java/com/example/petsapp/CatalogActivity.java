65e255e0c7409e88496308c7231f4e892e3271cd
Blindcrash <blindcrash65@gmail.com>
2019-03-06 01:12:21 -0500

    Added files for the Db

--- app/src/main/java/com/example/petsapp/CatalogActivity.java
+++ app/src/main/java/com/example/petsapp/CatalogActivity.java
@@ -1,6 +1,8 @@
 package com.example.petsapp;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.petsapp.data.PetContract.PetEntry;
+import com.example.petsapp.data.PetDbHelper;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -26,7 +32,37 @@ public class CatalogActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        displayDatabaseInfo();
     }
+
+    /**
+     * Temporary helper method to display information in the onscreen TextView about the state of
+     * the pets database.
+     */
+    private void displayDatabaseInfo() {
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        PetDbHelper mDbHelper = new PetDbHelper(this);
+
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Perform this raw SQL query "SELECT * FROM pets"
+        // to get a Cursor that contains all rows from the pets table.
+        Cursor cursor = db.rawQuery("SELECT * FROM " + PetEntry.TABLE_NAME, null);
+        try {
+            // Display the number of rows in the Cursor (which reflects the number of rows in the
+            // pets table in the database).
+            TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+            displayView.setText("Number of rows in pets database table: " + cursor.getCount());
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/petsapp/data/PetContract.java
+++ app/src/main/java/com/example/petsapp/data/PetContract.java
@@ -9,9 +9,9 @@ public final class PetContract {
 
         public final static String TABLE_NAME = "pets";
 
-        public final static String _ID = "pets";
-        public final static String COLUMN_PET_NAME = "pets";
-        public final static String COLUMN_PET_BREED = "name";
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PET_NAME = "name";
+        public final static String COLUMN_PET_BREED = "breed";
         public final static String COLUMN_PET_GENDER = "gender";
         public final static String COLUMN_PET_WEIGHT = "weight";
 
